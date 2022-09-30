04e5e06f7651b15bb662dc2ba1e2daa860bdf50c
Nick Garza <thenickgarza14@gmail.com>
2022-09-30 10:50:02 -0500

    addded .env for the the db password in server.js

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const { rsort } = require('semver');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// const path = require("path");
+require("dotenv").config();
+// requiring mysql 
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connection to the mysql database 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.DB_PW,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // test get route
 // app.get('/', (req, res) => {
@@ -13,6 +27,10 @@ app.use(express.json());
 //         message: 'Hello World'
 //     });
 // });
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not found)
 app.use((req, res) => {
